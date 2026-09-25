8a7ece7ba77ae6d0eb4935272cc74d50d524cfb0
pshimizu <naohiko.shimizu@gmail.com>
2026-09-25 14:44:27 +0900

    IRQ handling is not follow set_irq, because we don't call callirq in the crt0.s

--- SoC/programs/30timer.c
+++ SoC/programs/30timer.c
@@ -6,10 +6,6 @@
 #define TIMER_LD     0x08
 #define TIMER_INTREQ 0x80
 
-#define STACK_SZ     0x40
-
-unsigned char int_stack[STACK_SZ];
-
 int count;
 long time;
 
@@ -23,9 +19,10 @@ unsigned char foo () {
 
 
 int main () {
+extern int* irqvec;
 	SEI();
 	  time=50000000;
-	  set_irq(foo, int_stack, STACK_SZ);
+	  irqvec=(int*)foo;
 	  count=0;
 	  *LED=count;
 	  *(long*)TIMER = time;
